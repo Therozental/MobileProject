b49da081716d39d723b7c2898a0ff7ebececa2d5
Therozental <160230574+Therozental@users.noreply.github.com>
2024-12-09 00:43:25 +0200

    cards placeholders

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,16 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
 [Serializable]
 public class Card : MonoBehaviour
 {
     public int Value;
     public string Suit;
-
-    // public Card(int value, string suit)
-    // {
-    //     Value = value;
-    //     Suit = suit;
-    // }
+    
 }
