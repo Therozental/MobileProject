f88a45aea6042ece06a948a569fa6da0fff885ff
Therozental <160230574+Therozental@users.noreply.github.com>
2024-12-19 22:47:14 +0200

    Cardpack scriptable

--- Assets/Scripts/Store/Store.cs
+++ Assets/Scripts/Store/Store.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Store : MonoBehaviour
 {
+    public Button BuyButton;
+    public Player player;
     public List<CardPackScriptableobject> availablePacks; // List of card packs available in the store
-    public int playerPoints; // Player's available points
     public int playerCash; // Player's available cash
     public Deck playerDeck; // Reference to the player's deck
-    public Player player;
+    
+   
+
+    
+    
+    
     
 }
