614596ae25d111280a6b6bbb9254fc6cad791fba
Therozental <160230574+Therozental@users.noreply.github.com>
2024-12-20 09:23:04 +0200

    cardpack fixing

--- Assets/Scripts/Store/Store.cs
+++ Assets/Scripts/Store/Store.cs
@@ -6,16 +6,9 @@ using UnityEngine.UI;
 
 public class Store : MonoBehaviour
 {
-    public Button BuyButton;
+  //  public List<CardPackScriptableobject> availablePacks; // List of card packs available in the store
     public Player player;
-    public List<CardPackScriptableobject> availablePacks; // List of card packs available in the store
     public int playerCash; // Player's available cash
     public Deck playerDeck; // Reference to the player's deck
-    
-   
-
-    
-    
-    
     
 }
