51cc742ff5318ce178404801bda362f0e6a4ce42
Therozental <160230574+Therozental@users.noreply.github.com>
2024-12-21 13:09:15 +0200

    card purchase code

--- Assets/Scripts/Store/CardPack.cs
+++ Assets/Scripts/Store/CardPack.cs
@@ -5,4 +5,41 @@ using UnityEngine;
 public class CardPack : MonoBehaviour
 {
     [SerializeField] private CardPackScriptableobject ScriptableObject;
+    public Store store;
+
+    public void PurchaseCardPoints()
+    {
+        if (store.player.Points >= ScriptableObject.costInPoints) // if player has enough points
+        {
+            // remove the points from the player
+            store.player.Points -= ScriptableObject.costInPoints; 
+            Debug.Log($"your sum points:{store.player.Points}");
+        
+            // add the pack cards to the deck
+            for (int i = 0; i < ScriptableObject.packCards.Count; i++)
+            {
+                store.playerDeck.deck.Add(ScriptableObject.packCards[i]);
+                Debug.Log($"{ScriptableObject.packCards[i]} added to you deck");
+            }
+        }
+        Debug.Log("not enough point!");
+        return;
+    }
+    /*
+    public void PurchasePointsByCash()
+    {
+        if (store.player.Cash >= ScriptableObject.costInCash) // if player has enough points
+        {
+            // remove the cash from the player
+            store.player.Cash -= ScriptableObject.costInCash; 
+        
+            // add the pack cards to the deck
+            for (int i = 0; i < ScriptableObject.packCards.Count; i++)
+            {
+                store.playerDeck.deck.Add(ScriptableObject.packCards[i]);
+            }
+        }
+    
+    }
+    */
 }
--- Assets/Scripts/Store/Store.cs
+++ Assets/Scripts/Store/Store.cs
@@ -8,7 +8,6 @@ public class Store : MonoBehaviour
 {
   //  public List<CardPackScriptableobject> availablePacks; // List of card packs available in the store
     public Player player;
-    public int playerCash; // Player's available cash
     public Deck playerDeck; // Reference to the player's deck
     
 }
